044f3d1cb5554a24dac43c57cd9359cec9cd4b06
Khalied <khaliedmails@gmail.com>
2025-07-19 16:33:52 +0300

    first commit

---
+++ src/game/constants.ts
@@ -0,0 +1,5 @@
+export const CONSTANTS = {
+    WINDOW_WIDTH: 1089,
+    WINDOW_HEIGHT: 792,
+
+}
---
+++ src/game/main.ts
@@ -0,0 +1,43 @@
+import { Boot } from './scenes/Boot';
+import { GameOver } from './scenes/GameOver';
+import { Game as MainGame } from './scenes/Game';
+import { MainMenu } from './scenes/MainMenu';
+import { AUTO, Game } from 'phaser';
+import { Preloader } from './scenes/Preloader';
+import { CONSTANTS } from './constants';
+
+//  Find out more information about the Game Config at:
+//  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
+const config: Phaser.Types.Core.GameConfig = {
+    type: AUTO,
+    width: CONSTANTS.WINDOW_WIDTH,
+    height: CONSTANTS.WINDOW_HEIGHT,
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x: 0, y: 0 },
+            debug: false
+        }
+    },
+    scene: [
+        Boot,
+        Preloader,
+        MainMenu,
+        MainGame,
+        GameOver
+    ]
+};
+
+const StartGame = (parent: string) => {
+
+    return new Game({ ...config, parent });
+
+}
+
+export default StartGame;
